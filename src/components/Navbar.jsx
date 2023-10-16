f5527792231c3ab35743a498b3cf8eb6f468c801
user.walid1921 <user.kouiderayadwalid@gmail.com>
2023-10-16 12:41:56 +0300

    fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef, useContext } from 'react';
+import React, { useState, useRef, useContext } from 'react';
 import AddRecipe from './UI/AddRecipe';
 import BookmarkContext from './BookmarkContext ';
 
--- src/components/UI/NumCharacters.jsx
+++ src/components/UI/NumCharacters.jsx
@@ -1,7 +1,12 @@
+import { FiSmile } from "react-icons/fi"
+
 function NumCharacters() {
   return (
-    <div className="  text-center mt-6 flex flex-col justify-center h-full">
-      <p className='text-color-primary text-lg'>Start by searching for a recipe</p>
+    <div className="mt-6 flex flex-col justify-center items-center h-full">
+
+      <p className='text-color-primary text-lg flex items-center gap-3'>
+      <FiSmile size={25}  />
+        Start by searching for a recipe</p>
       <span className='text-[#c0bfbf] text-sm'>(Please enter at least 4 characters)</span>
     </div>
   )
